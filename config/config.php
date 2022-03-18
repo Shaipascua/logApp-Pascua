f13709b9191f353dd1079a2ab6ae9f039dcdbde6
unknown <201980098@psu.palawan.edu.ph>
2022-03-18 18:44:35 +0800

    changes made

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-	define('ROOT_URL', '   ');
+	define('ROOT_URL', '');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6479644');
 	define('DB_PASS', 'M3e4jEV338');
 	define('DB_NAME', 'sql6479644');
 	
-?>
+?
